bb54c572e0af08951417c23f84525c685d2f8a59
dennisnuriman <dennismuhammad.md@gmail.com>
2022-04-02 17:55:46 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:coba/pages/landing_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingPage(),
+    );
+  }
+}
